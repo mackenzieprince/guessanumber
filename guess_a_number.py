7ed4dccdbf86000730c1a790404e448eaf28917f
mackenzieprince <mackenzieprince4@gmail.com>
2017-05-22 16:25:43 -0700

    Update guess_a_number.py

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,25 +1,3 @@
-import random
-
-# 1. Ask if they want to play the game : play()
-#     a/ yes....guess()
-#         pick a number
-#     b. no....exit()
-
-
-
-# 2. Ask them to guess a number : guess()
-# guessed_number_list.append(guess)
-# check()
-
-# 3. Check the number : check(number, guessed_number_list)
-#     a. if match print result play()
-#     b. if not match give input guess()
-#         if abs(number - guess) < abs(number - guess_before)...then, say getting warmer
-#         guessed_numbr = guessed_number_list[-1]
-#         guessed_before = guessed_number_list[-2]
-
-
-
 def play():
     guessed_number_list = []
     num_games_played = 0
@@ -62,14 +40,13 @@ def guess_a_num(secret_number,guessed_number_list):
 
 def directions():
     print "Would you like to play Beginner level (B) or Advanced level (A)? n/Beginnger Level: I'm thinking of a number between 1 and 50. You have an unlimited number of guesses! /n Advanced level: I'm thinking of a number between 1 and 100. You will have 5 chances to guess the number. "
-        if raw_input("B")
-        play_beginner()
-    elif: raw_input("A")
-        play_advanced()
+        if raw_input("B"):
+            upper_range = 50
+            number_of_guesses_allowed = None
+            play(upper_range,number_of_guesses_allowed)
+    elif raw_input("A"):
+        
 
 
-def play_beginner(secret_number,guessed_number_list)
-    guess = raw_input("Please guess a number between 1 and 50.  ")
 
-
-play()
+directions()
